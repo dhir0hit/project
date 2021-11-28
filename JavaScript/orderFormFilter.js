3c3c1d6396c87d7a7c40656b0ae77d73782c7016
dhir0hit <rohit786dhir@gmail.com>
2021-11-27 22:48:19 -0500

    mainmenu and order

--- JavaScript/orderFormFilter.js
+++ JavaScript/orderFormFilter.js
@@ -1,5 +1,7 @@
 function orderFormFilter () {
-    var item_array = {
-
-    }
+    var item_array = ["no_order", "Risotto_Alla_Milanese", "Polenta", "Lasagna",
+        "Ravioli", "Osso_Buco", "Arancini", "Ribollita", "Spaghetti_Alla_Carbonara",
+        "Anhui", "Cantonese", "Fujian", "Hunan", "Jiangsu", "Shandong", "Szechuan",
+        "Zhejiang", "Affogato", "Americano", "Caffe_Latte", "Caffe_Mocha",
+        "Caffe_Au_Lait", "Cappuccino"]
 }
