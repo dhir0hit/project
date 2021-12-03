09f5a6a7f5f1614403c07123ade19a828e2ba6d0
dhir0hit <rohit786dhir@gmail.com>
2021-12-02 20:51:23 -0500

    Updated menu and next adding signup

--- JavaScript/Search Filter.js
+++ JavaScript/Search Filter.js
@@ -14,15 +14,15 @@ function filterFunction() {
         }
     }
 }
-localStorage.clear();
+// localStorage.clear();
 
-window.onload = function () {
-    Items = ["Arancini", "Lasanga", "Osso", "Polenta", "Ravioli", "Ribollita", "Risotto", "Spagetti",
-        "Anhui", "Cantonese", "Fujian", "Hunan", "Jiangsu", "Shandong", "Szechuan", "Zhejiang",
-        "Affogato", "Americano", "Caffe_Latte", "Caffe_Mocha", "Caffe_Au_Lait", "Cappuccino", "Cold_Brew_Coffee", "Double_espresso",
-        "AlmondMalaiKulfi", "ApplePie", "ChocolateCoffeeTruffle", "CoconutKheer", "FudgyChewyBrownies", "LemonTart", "LowFatTiramisu", "PistachioPhirni"
-    ]
-    for (var i=0; i < Items.length; i++) {
-        localStorage.setItem(Items[i], '0');
-    }
-}
+// window.onload = function () {
+//     Items = ["Arancini", "Lasanga", "Osso", "Polenta", "Ravioli", "Ribollita", "Risotto", "Spagetti",
+//         "Anhui", "Cantonese", "Fujian", "Hunan", "Jiangsu", "Shandong", "Szechuan", "Zhejiang",
+//         "Affogato", "Americano", "Caffe_Latte", "Caffe_Mocha", "Caffe_Au_Lait", "Cappuccino", "Cold_Brew_Coffee", "Double_espresso",
+//         "AlmondMalaiKulfi", "ApplePie", "ChocolateCoffeeTruffle", "CoconutKheer", "FudgyChewyBrownies", "LemonTart", "LowFatTiramisu", "PistachioPhirni"
+//     ]
+//     for (var i=0; i < Items.length; i++) {
+//         localStorage.setItem(Items[i], '0');
+//     }
+// }
